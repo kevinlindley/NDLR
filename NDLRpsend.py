39fdbfd3b47f7f5d3e2a6da3f5fd0da755642c16
Kevin Lindley <kevin.lindley@ntlworld.com>
2023-12-19 20:45:42 +0000

    Added NDLRpsend

---
+++ NDLRpsend.py
@@ -0,0 +1,30 @@
+#
+#  Author      : Kevin Lindley
+#  Date        : 2023-12-19
+#  Version     : 1.1.0
+#  Filename    : NDLRpsend.py
+#  Description : Send a patterns from stdin to the NDLR's COM port
+#  Usage       : python NDLRpsend.py < NDLR_Patt_Blank.txt
+#
+#
+import sys
+import serial
+#
+#Change the Serial Port to the one used by you system in the variable below
+#
+COM_PORT = "COM7"
+NDLR_ba = bytearray()
+#
+# Connect to your Serial Port
+#
+print("Sending Patterns file to NDLR [",end="")
+serialPort = serial.Serial(port=COM_PORT, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+for line in sys.stdin:
+    print(".",end="")
+    NDLR_ba.extend(map(ord, line))
+    serialPort.write(NDLR_ba)
+print("] Done")
+#
+# Disconnect
+#
+serialPort.close()
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Python Experiments with the NDLR Sequencer
 
 ## ndlr_reset_all_chord_seq_presets.py
   Simple script that resets all the NDLR's Chord Sequences to Empty (0's)
+
+## Patterns Folder
+  Contains a list of Patterns that can be loaded into the NDLR via the COM interface
